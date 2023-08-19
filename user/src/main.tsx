48871fe2a7b9493a9f46bcf1bf65897c09bb5538
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-08-19 21:03:18 +0500

    chore: `react-query`, `axios ` added and useFetch hook initialized

--- user/src/main.tsx
+++ user/src/main.tsx
@@ -1,11 +1,30 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux"
-import App from './App'
-import {store} from '@/redux'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import './index.css'
+
+import App from './App'
+import { store } from '@/redux'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: 1,
+      staleTime: 5 * 1000,
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </Provider>
 )
---
+++ user/src/utils/api.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+const API_URL =
+  import.meta.env.APP_MODE === "production"
+    ? import.meta.env.VITE_PRODUCTION_API_URL
+    : import.meta.env.VITE_STAGING_API_URL; // 'development' and 'staging' mode both makes requests to staging API
+
+export const http = axios.create({
+  baseURL: API_URL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
+
+export function useFetch<T>(key: (string | number)[], url: string) {
+  const data = useQuery<T>({
+    queryKey: key,
+    queryFn: () => http.get(url),
+  });
+
+  return data;
+}
