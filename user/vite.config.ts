0c53da77a2763b7ba95f36b9f0756c25782b1947
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-10-01 19:23:17 +0500

    WIP

--- user/vite.config.ts
+++ user/vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from "path"
 
+const ASSET_URL = process.env.ASSET_URL || '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${ASSET_URL}`,
   plugins: [react()],
   resolve: {
     alias: {
