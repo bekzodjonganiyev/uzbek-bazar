5127d63ccbcb7191762ea6d43f886b097979620e
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-10-01 18:49:22 +0500

    fix: Base url

--- user/src/utils/api.ts
+++ user/src/utils/api.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useQuery, useMutation } from "@tanstack/react-query";
 
-export const API_URL =
-  import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_PRODUCTION_API_URL
-    : import.meta.env.VITE_STAGING_API_URL; // 'development' and 'staging' mode both makes requests to staging API
-    
+export const API_URL = import.meta.env.VITE_STAGING_API_URL;
+  // import.meta.env.MODE === "production"
+  //   ? import.meta.env.VITE_PRODUCTION_API_URL
+  //   :  // 'development' and 'staging' mode both makes requests to staging API
+
+console.log(import.meta.env.VITE_STAGING_API_URL)
 export const http = axios.create({
   baseURL: API_URL,
   headers: {
--- user/vite.config.ts
+++ user/vite.config.ts
@@ -4,9 +4,6 @@ import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    origin: 'https://uzb.technostudio.uz/api/v1/web',
-  },
   plugins: [react()],
   resolve: {
     alias: {
