b39de747fe9395c25a3cf8dfc1025d4705187cdd
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-10-01 19:32:55 +0500

    wip

--- user/src/utils/api.ts
+++ user/src/utils/api.ts
@@ -1,17 +1,19 @@
 import axios from "axios";
 import { useQuery, useMutation } from "@tanstack/react-query";
 
+const A = import.meta.env
+console.log(A, "this is url")
 const API_URL =
   import.meta.env.PROD
     ? import.meta.env.VITE_PRODUCTION_API_URL
     : import.meta.env.VITE_STAGING_API_URL; // 'development' and 'staging' mode both makes requests to staging API
 
-console.log(import.meta.env);
-console.log(import.meta.env.BASE_URL);
-console.log(import.meta.env.VITE_APP_MODE);
-console.log(import.meta.env.VITE_PRODUCTION_API_URL);
-console.log(import.meta.env.VITE_STAGING_API_URL);
-console.log(import.meta.env.PROD);
+// console.log(import.meta.env);
+// console.log(import.meta.env.BASE_URL);
+// console.log(import.meta.env.VITE_APP_MODE);
+// console.log(import.meta.env.VITE_PRODUCTION_API_URL);
+// console.log(import.meta.env.VITE_STAGING_API_URL);
+// console.log(import.meta.env.PROD);
 
 export const http = axios.create({
   baseURL: API_URL,
--- user/vite.config.ts
+++ user/vite.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from "path"
 
-const ASSET_URL = process.env.ASSET_URL || '';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `${ASSET_URL}`,
   plugins: [react()],
   resolve: {
     alias: {
