d02ef9f661f0597480168da38296a7f14f3d8abb
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-11-22 11:33:46 +0500

    fix: getSessionId hook's get uerId functionality reverted

--- user/src/utils/getSeesionId.ts
+++ user/src/utils/getSeesionId.ts
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-import { http } from "./api";
+// import { http } from "./api";
 
 export function getMachineId() {
   let machineId = localStorage.getItem("MachineId");
-  let token = localStorage.getItem("token");
+  // let token = localStorage.getItem("token");
 
-  const [userId, setUserId] = useState(null);
-  const [truthlyToken, setTruthlyToken] = useState<boolean>(false);
+  // const [userId, setUserId] = useState(null);
+  // const [truthlyToken, setTruthlyToken] = useState<boolean>(false);
 
   // MachineId is recreated if not found in localStorage
   if (!machineId) {
@@ -16,20 +16,20 @@ export function getMachineId() {
   }
 
   // Token checked for correct
-  useEffect(() => {
-    (async function () {
-      const res = await http(true).get("/clients/profile/");
-      if (res.status === 200) {
-        setTruthlyToken(true);
-        setUserId(res.data.id);
-      }
-    })();
-  }, [token]);
+  // useEffect(() => {
+  //   (async function () {
+  //     const res = await http(true).get("/clients/profile/");
+  //     if (res.status === 200) {
+  //       setTruthlyToken(true);
+  //       setUserId(res.data.id);
+  //     }
+  //   })();
+  // }, [token]);
 
   // If token is correct or has in localStorage return userId else machineId
-  if (truthlyToken) {
-    return userId;
-  } else {
+  // if (truthlyToken) {
+  //   return userId;
+  // } else {
     return machineId;
-  }
+  // }
 }
